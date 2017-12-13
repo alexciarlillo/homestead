a282064bba5355b6a201818c51c8dc95a55d4fb4
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2017-12-13 15:02:56 -0500

    add CELS specific environment settings

--- resources/after.sh
+++ resources/after.sh
@@ -3,3 +3,14 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+# allow remote redis access
+sed -i '/^bind/s/bind 127.0.0.1/bind 127.0.0.1 192.168.10.10/' /etc/redis/redis.conf
+
+# add CELS specific db settings
+cat >> /etc/mysql/my.cnf << EOF
+
+[mysqld]
+sql_mode = "NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+group_concat_max_len = 1048576
+EOF
